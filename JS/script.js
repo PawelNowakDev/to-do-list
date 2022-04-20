bebb1ac74c8a83eb7a6a4d2c072c01a448d5af62
Pawel Nowak <pakanowak@gmail.com>
2022-04-20 23:37:46 +0200

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,69 @@
+{
+    const tasks = [{
+            content: "zrobić pracę domową",
+            done: false
+        },
+        {
+            content: "zrobić trening",
+            done: true
+        }
+    ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent
+        });
+
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+              <li
+              ${task.done ? ' style="text-decoration: line-through"' : ""}
+              >
+              <button class="js-remove">usuń</button>
+              ${task.content}
+              </li>
+              `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
