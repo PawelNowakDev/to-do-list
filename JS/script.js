a2d355d670556f21decad94a640de475c2b1ac3c
Pawel Nowak <pakanowak@gmail.com>
2022-04-27 22:12:00 +0200

    remove and toggle task done function added

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 {
     const tasks = [{
-            content: "zrobić pracę domową",
+            content: "nagrać lekcję",
             done: false
         },
         {
-            content: "zrobić trening",
+            content: "zjeść pierogi",
             done: true
         }
     ];
@@ -17,8 +17,13 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     };
 
@@ -27,21 +32,31 @@
 
         for (const task of tasks) {
             htmlString += `
-              <li
-              ${task.done ? ' style="text-decoration: line-through"' : ""}
-              >
-              <button class="js-remove">usuń</button>
-              ${task.content}
-              </li>
-              `;
+        <li
+        ${task.done ? ' style="text-decoration: line-through"' : ""}
+        >
+        <button class="js-done">zrobione?</button>
+        <button class="js-remove">usuń</button>
+        ${task.content}
+        </li>
+        `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
         const removeButtons = document.querySelectorAll(".js-remove");
+
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
@@ -50,7 +65,6 @@
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
         if (newTaskContent === "") {
             return;
         }
@@ -61,7 +75,6 @@
         render();
 
         const form = document.querySelector(".js-form");
-
         form.addEventListener("submit", onFormSubmit);
     };
 
